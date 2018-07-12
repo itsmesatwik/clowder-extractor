b5470c9ceaf8b3050c08c24a15816a6a89ba7a74
Satwik <satwik.illini@gmail.com>
2018-07-12 16:52:43 +0000

    remove old stuff

--- pyKaldi/KaldiExtractor.py
+++
@@ -1,9 +0,0 @@
-from kaldi.asr import Recognizer
-from kaldi.decoder import FasterDecoder, FasterDecoderOptions
-from kaldi.feat.mfcc import Mfcc, MfccOptions
-from kaldi.feat.functions import compute_deltas, DeltaFeaturesOptions
-from kaldi.fstext import SymbolTable, read_fst_kaldi
-from kaldi.gmm.am import AmDiagGmm, DecodableAmDiagGmmScaled
-from kaldi.hmm import TransitionModel
-from kaldi.util.io import xopen
-from kaldi.util.table import SequentialWaveReader
